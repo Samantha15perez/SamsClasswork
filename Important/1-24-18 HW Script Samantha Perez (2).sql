5fa0b5832171b323d28153e20d0126fc1d17a947
Samantha15perez <Samantha15perez@gmail.com>
2018-01-30 21:32:16 -0500

    Done

---
+++ Important/1-24-18 HW Script Samantha Perez (2).sql
@@ -0,0 +1,378 @@
+USE MASTER;
+
+IF (Select COUNT(*) from sys.databases where name = 'VeterinaryDB') >0
+
+ BEGIN
+ DROP DATABASE VeterinaryDB
+END
+
+ CREATE DATABASE VeterinaryDB;
+ 
+ GO
+
+ Use VeterinaryDB;
+
+ PRINT '-- Database Reset';
+
+     IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'Clients') > 0 
+ BEGIN
+ DROP TABLE Clients
+ END 
+ 
+ CREATE TABLE Clients
+ (
+ ClientID INT NOT NULL Identity(1,1),
+ FirstName Varchar(25) NOT NULL, 
+ LastName Varchar(25) NOT NULL,
+ MiddleName Varchar(25),
+ CreateDate DATE
+ PRIMARY KEY (ClientID)
+ )
+
+ Print '--  Clients Table Created'
+
+
+  IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'ClientContacts') > 0 
+ BEGIN
+ DROP TABLE ClientContacts
+ END 
+ 
+ CREATE TABLE ClientContacts
+ (
+ AddressID INT NOT NULL Identity(1,1),
+ ClientID INT Not NULL,
+ AddressType INT NOT NULL,
+ AddressLine1 Varchar(50) NOT NULL,
+ Addressline2 Varchar(50),
+ City Varchar(35) NOT NULL,
+ StateProvince Varchar(25) NOT NULL,
+ PostalCode Varchar(15) NOT NULL,
+ Phone Varchar(15) NOT NULL,
+ AltPhone Varchar(15) NOT NULL,
+ Email Varchar(35) NOT NULL
+ PRIMARY KEY (AddressID)
+ CONSTRAINT FK_ClientContacts_Clients FOREIGN KEY (ClientID) REFERENCES Clients(ClientID)
+ )
+
+  PRINT '--  ClientContacts Table Created'
+
+  INSERT INTO Clients (Firstname, Lastname, Middlename, CreateDate)
+  VALUES 
+  ('Dave', 'Strider', 'Arthur', getdate()),
+  ('Rose', 'Lalonde', NULL, getdate()),
+  ('John', 'Egbert', 'James', getdate()),
+  ('Jade', 'Harley', NULL, getdate()),
+  ('Jake', 'English', NULL, getdate())
+
+  PRINT '--  Clients Table Populated'
+  INSERT INTO ClientContacts (Clientid, Addresstype, Addressline1, Addressline2, City, Stateprovince, Postalcode, Phone, Altphone, Email)
+  VALUES 
+  ((Select Clientid from CLients where lastname = 'Strider'), 1, '1234 test Blvd', NULL, 'Silver Springs', 'FL', '32134', '(352) 299-3716', '(352) 856-9233', 'DStride@Gmail.com'),
+  ((Select Clientid from CLients where lastname = 'Lalonde'), 2, '7257 Radial Ct', NULL, 'Ocala', 'FL', '34470', '(352) 725-3886', '(352) 826-0742', 'Roxxy96@hotmail.com'),
+  ((Select Clientid from CLients where lastname = 'Egbert'), 1, '102 Buena Vista Blvd', NULL, 'Ocala', 'FL', '34472', '(352) 713-6244', '(352) 723-1264', 'Egghead@gmail.com'),
+  ((Select Clientid from CLients where lastname = 'Harley'), 1, '303 SW Wenona Ave', NULL, 'Ocala', 'FL', '34474', '(352) 162-7723', '(352) 826-2009', 'DoggoFriend@mail.com'),
+  ((Select Clientid from CLients where lastname = 'English'), 1, '112 Adventure Way', NULL, 'Ocala', 'FL', '34471', '(352) 920-1524', '(352) 141-1569', 'DualWieldingPistols@hotmail.com')
+
+  PRINT '--  ClientContacts Table Populated'
+
+
+
+        IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'Employees') > 0 
+ BEGIN
+ DROP TABLE Employees
+ END 
+
+ CREATE TABLE Employees
+ (
+ EmployeeID INT IDENTITY(1,1),
+ LastName varchar(25) NOT NULL,
+ Firstname varchar(25) NOT NULL,
+ MiddleName varchar(25),
+ Hiredate DATE not null,
+ Title varchar(50) not null
+ PRIMARY KEY (EmployeeID)
+ )
+
+
+  PRINT '--  Employees Table Created'
+
+
+      IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'EmployeeContactInfo') > 0 
+ BEGIN
+ DROP TABLE EmployeeContactInfo
+ END 
+
+ CREATE TABLE EmployeeContactInfo
+ (
+ AddressID INT Identity(1,1),
+ AddressLine1 Varchar(50) NOT NULL,
+ Addressline2 Varchar(50),
+ City Varchar(35) NOT NULL,
+ StateProvince Varchar(25) NOT NULL,
+ PostalCode Varchar(15) NOT NULL,
+ Phone Varchar(15) NOT NULL,
+ AltPhone Varchar(15) NOT NULL,
+ EmployeeID INT NOT NULL
+ PRIMARY KEY (AddressID)
+ CONSTRAINT FK_EmployeeContactInfo_Employees FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID)
+ )
+
+ PRINT '--  EmployeeContactInfo Table Created'
+
+INSERT INTO Employees (Lastname, Firstname, MiddleName, Hiredate, Title)
+Values 
+('Smith', 'John', NULL, '07-29-1998', 'Veterinarian'),
+('Conrad', 'Holly', NULL, '12-15-2012', 'Avian Specialist'),
+('Newman', 'Chelsea', NULL, '08-08-2005', 'Assistant'),
+('Burke', 'Victoria', 'Shawn', '01-23-2002', 'Radiologist'),
+('Beck', 'Percy', 'Levi', '11-30-2009', 'Veterinary Surgeon'),
+('Stone', 'Andy', 'Frank', '05-12-2013', 'Parasitologist'),
+('Hale', 'Allen', 'Bailey', '04-28-2011', 'Veterinarian')
+
+ PRINT '--  Employees Table Populated'
+
+ INSERT INTO EmployeeContactInfo (AddressLine1, AddressLine2, City, StateProvince, PostalCode, Phone, AltPhone, EmployeeID)
+ VALUES 
+ ('3266 Bagwell Avenue', NULL, 'Ocala', 'FL', '34471', '352-804-1718', '352-266-8720', (Select EmployeeID from employees WHERE lastname = 'Smith')),
+ ('3156 Long Street', NULL, 'Ocala', 'FL', '34471', '352-201-2330', '352-875-8270', (Select EmployeeID from employees WHERE lastname = 'Conrad')),
+ ('3465 Rhapsody Street', NULL, 'Ocala', 'FL', '34470', '352-266-7354', '352-572-2181', (Select EmployeeID from employees WHERE lastname = 'Newman')),
+ ('1793 Grant View Drive', NULL, 'Ocala', 'FL', '34471', '352-276-9976', '352-836-5853', (Select EmployeeID from employees WHERE lastname = 'Burke')),
+ ('250 Olen Thomas Drive', NULL, 'Ocala', 'FL', '34472', '352-233-7357', '352-424-3634', (Select EmployeeID from employees WHERE lastname = 'Beck')),
+ ('2771 Broaddus Avenue', NULL, 'Ocala', 'FL', '34470', '352-757-7572', '352-342-2334', (Select EmployeeID from employees WHERE lastname = 'Stone')),
+ ('1020 Charack Road', NULL, 'Ocala', 'FL', '34474', '352-123-0562', '352-424-2888', (Select EmployeeID from employees WHERE lastname = 'Hale'))
+
+ PRINT '--  EmployeeContactInfo Table Populated'
+
+
+          IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'AnimalTypeReference') > 0 
+ BEGIN
+ DROP TABLE AnimalTypeReference
+ END 
+
+ CREATE TABLE AnimalTypeReference
+ (
+ AnimalTypeID INT IDENTITY(1,1),
+ Species Varchar(35) NOT NULL,
+ Breed Varchar(35) NOT NULL
+ PRIMARY KEY (AnimalTypeID)
+ )
+
+
+ 
+ PRINT '--  AnimalTypeReference Table Created'
+
+ INSERT INTO Animaltypereference (Species, Breed)
+ VALUES
+('Horse', 'Pasofino'),
+('Parrot','Parakeet'),
+('Hamster','Russian'),
+('Cat','Scottish Fold'),
+('Cat','Siamese'),
+('Tortoise','Leopard Tortoise'),
+('Dog','Samoyed')
+
+ PRINT '--  AnimalTypeReference Table Populated'
+
+
+        IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'Patients') > 0 
+ BEGIN
+ DROP TABLE Patients
+ END 
+
+ CREATE TABLE Patients
+ (
+PatientID INT IDENTITY(1,1),
+ClientID INT NOT NULL,
+PatientName Varchar(35) NOT NULL,
+AnimalType INT NOT NULL,
+Color Varchar(25),
+Gender Varchar(2) NOT NULL,
+BirthYear Varchar(4),
+[Weight] Decimal(6) NOT NULL,
+[Description] varchar(1024), 
+GeneralNotes varchar(2048) NOT NULL,
+Chipped BIT NOT NULL, 
+RabiesVacc datetime
+PRIMARY KEY (PatientID)
+ CONSTRAINT FK_Patients_Clients FOREIGN KEY (clientID) REFERENCES Clients(ClientID),
+ CONSTRAINT FK_Patients_AnimalType FOREIGN KEY (AnimalType) REFERENCES AnimalTypeReference(AnimalTypeID)
+ )
+
+
+ 
+ PRINT '--  Patients Table Created'
+
+INSERT INTO Patients (Clientid, Patientname, animaltype, color, gender, birthyear, [weight], [description], generalnotes, chipped, rabiesvacc)
+VALUES
+((Select Clientid from CLients where lastname = 'Strider'), 'Pandamonium', 7, 'White/Black', 'M', '2013', '88.7', 'Large dog, Mostly White with Panda-Like Markings', 'Large dog. Non-Aggressive. Peanut Allergy.', 1, getdate()),
+((Select Clientid from CLients where lastname = 'Strider'), 'Lord Voldetort', 6, 'Standard', 'M', '1998', '8.4', 'Medium Sized Tortoise, Light Coloration', 'Very Aggressive. Handle with care.', 0, '09-09-2001'),
+((Select Clientid from CLients where lastname = 'Lalonde'), 'Socket Wrench', 5, 'Siamese', 'F', '2010', '10.2', NULL, 'Overweight female. Good temperment but food aggressive.', 1, '12-21-2011'),
+((Select Clientid from CLients where lastname = 'Lalonde'), 'Antonio', 2, 'Blue', 'M', '2007', '.132', NULL, 'Anxious Male. Prone to Feather-Pulling.', 0, '03-29-2009'),
+((Select Clientid from CLients where lastname = 'Egbert'), 'Jeremy', 4, 'Orange Tabby', 'M', '2014', '8.9', NULL, 'Juvenile Male, non-aggressive', 1, '11-30-2015'),
+((Select Clientid from CLients where lastname = 'Harley'), 'Poptart', 3, 'Gray', 'F', '2015', '.24', NULL, 'Previous Abcess on inner right cheek', 0, '08-24-2015'),
+((Select Clientid from CLients where lastname = 'English'), 'Luna', 1, 'Brown', 'F', '2007', '850', NULL, 'Unbred Female', 0, '09-12-2008') 
+
+
+
+
+ PRINT '--  Patients Table Populated'
+
+
+
+
+         IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'Visits') > 0 
+ BEGIN
+ DROP TABLE Visits
+ END 
+
+ CREATE TABLE Visits
+ (
+ VisitID INT Identity(1,1),
+ StartTime datetime NOT NULL,
+ Endtime datetime NOT NULL,
+ Appointment BIT NOT NULL,
+ DiagnosisCode varchar(12) NOT NULL,
+ ProcedureCode varchar(12) NOT NULL,
+ VisitNotes varchar(2048) NOT NULL,
+ PatientID Int NOT NULL,
+ EmployeeID INT NOT NULL
+ PRIMARY KEY (VisitID)
+ CONSTRAINT FK_Visits_Employees FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+ CONSTRAINT CK_Starttime_endtime CHECK (Endtime > Starttime)
+ )
+
+
+ PRINT '--  Visits Table Created'
+
+INSERT INTO Visits (Starttime, Endtime, Appointment, Diagnosiscode, Procedurecode, VisitNotes, PatientID, EmployeeID)
+VALUES
+('2017-01-02 12:00:00', (SELECT dateadd(mi, (rand() * 100), '2017-01-02 12:00:00')), 1, '36E79', '00ED2', 'Routine Wellness Check', 6, 1),
+('2017-05-07 11:00:00', (SELECT dateadd(mi, (rand() * 100), '2018-05-07 11:00:00')), 1, '82BK9', '28H82', 'Patient has sore paw', 3, 7), 
+('2017-05-21 12:00:00', (SELECT dateadd(mi, (rand() * 100), '2018-05-21 12:00:00')), 1, '36E79', '02ED3', 'Routine Wellness Check: Patient diagnosed with athsma', 4, 7),
+('2017-09-24 12:00:00', (SELECT dateadd(mi, (rand() * 1000), '2018-09-24 12:00:00')), 0, '86GH2', '92H12', 'Emergency visit: Patient diagnosed with Colic; promptly treated', 1, 1),
+('2018-11-12 12:00:00', (SELECT dateadd(mi, (rand() * 100), '2018-11-12 12:00:00')), 1, 'H12H9', 'QA0AH', 'Standard Appt: Sprained tail from wagging too hard', 7, 7)
+
+
+
+
+
+ PRINT '--  Visits Table Populated'
+ 
+
+
+      IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'Billing') > 0 
+ BEGIN
+ DROP TABLE Billing
+ END 
+
+ CREATE TABLE Billing
+ (
+ BillID INT Not Null Identity(1,1),
+ Billdate DATE NOT NULL,
+ ClientID INT NOT NULL,
+ VisitID INT Not null,
+ Amount decimal NOT NULL
+ PRIMARY KEY (BillID)
+ CONSTRAINT FK_Billing_Clients FOREIGN KEY (ClientID) REFERENCES Clients(ClientID),
+ CONSTRAINT FK_Billing_Visits FOREIGN KEY (VisitID) REFERENCES Visits(VisitID)
+ )
+
+  PRINT '--  Billing Table Created'
+
+INSERT INTO Billing (Billdate, clientid, visitid, amount)
+VALUES 
+('2017-01-02', (Select Clientid from CLients where lastname = 'Strider'), 1, '40.00'),
+('2017-05-07', (Select Clientid from CLients where lastname = 'Harley'), 2, '57.73'),
+('2017-05-21', (Select Clientid from CLients where lastname = 'Egbert'), 3, '39.31'),
+('2017-09-24', (Select Clientid from CLients where lastname = 'English'), 4, '187.49'),
+('2018-11-12', (Select Clientid from CLients where lastname = 'Strider'), 5, '49.97')
+
+ PRINT '--  Billing Table Populated'
+
+
+
+
+   IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'Payments') > 0 
+ BEGIN
+ DROP TABLE Payments
+ END 
+
+ CREATE TABLE Payments 
+ (
+ PaymentID INT NOT NULL IDENTITY(1,1),
+ PaymentDate DATE NOT NULL,
+ BillID INT,
+ Notes Varchar(2048),
+ Amount decimal NOT NULL
+ PRIMARY KEY (PaymentID)
+ )
+
+ PRINT '--  Payments Table Created'
+
+INSERT INTO Payments (Paymentdate, billid, notes, amount)
+VALUES 
+((select billdate from billing where billid = 1), 1, 'Paid on time', (select amount from billing where billid = 1)),
+((select billdate from billing where billid = 2), 1, 'Paid on time', (select amount from billing where billid = 2)),
+((select billdate from billing where billid = 3), 1, 'Paid on time', (select amount from billing where billid = 3)),
+((select billdate from billing where billid = 4), 1, 'Paid on time', (select amount from billing where billid = 4)),
+((select billdate from billing where billid = 5), 1, 'Paid on time', (select amount from billing where billid = 5))
+
+
+
+ PRINT '--  Payments Table Populated'
+
+ /*
+
+  CREATE LOGIN VetManager
+  WITH PASSWORD = 'VTMAN3357';
+  GO
+    
+  CREATE USER VetManager FOR LOGIN VetManager;
+  GO
+
+  CREATE LOGIN VetClerk
+  WITH PASSWORD 'ClerkV0829';
+  GO 
+
+  ALTER ROLE db_datareader ADD MEMBER VetManager;
+  GO
+
+  ALTER ROLE db_datawriter ADD MEMBER VetManager;
+  GO
+
+  ALTER ROLE db_datareader ADD MEMBER VetClerk;
+  GO
+
+  DENY ALTER ON OBJECT::
+  Clientcontacts
+  TO Vetclerk
+
+  DENY SELECT ON OBJECT::
+  Clientcontacts
+  TO Vetclerk
+
+  DENY UPDATE ON OBJECT::
+  Clientcontacts
+  TO Vetclerk
+
+  DENY INSERT ON OBJECT:: 
+   Clientcontacts
+  TO Vetclerk
+
+  DENY DELETE ON OBJECT::
+   Clientcontacts
+  TO Vetclerk
+
+ GRANT EXEC ON OBJECT::
+ sp_species 
+ TO VETCLERK
+
+ GRANT EXEC ON OBJECT::
+ SP_billing
+ to vetclerk
+
+
+  PRINT '-- Created User Logins'
+
+
+  */ 
---
+++ Important/1-30-18 CLASSWORK.sql
@@ -0,0 +1,206 @@
+use adventureworks2012
+
+----1
+
+--SELECT concat(p.firstname, ' ',p.lastname) [Name]
+--from humanresources.employee e
+--Inner join person.Person P
+--on p.BusinessEntityID = e.BusinessEntityID
+--INTERSECT
+--SELECT concat(p.firstname, ' ',p.lastname) [Name]
+--from sales.salesperson SP
+--Inner join person.Person P
+--on p.BusinessEntityID = sp.BusinessEntityID
+
+--2
+
+
+--BEGIN TRANSACTION
+
+--insert into person.BusinessEntity (modifieddate)
+--values (getdate())
+
+--Insert into person.person (businessentityid, persontype, namestyle, firstname, middlename, lastname, emailpromotion)
+--VALUES ('20778', 'EM', 0, 'Sam', 'N', 'Perez', 0)
+
+--Insert into HumanResources.Employee (BusinessEntityID, NationalIdNumber, LoginID, Jobtitle, Birthdate, Maritalstatus,
+--gender, hiredate, salariedflag, vacationhours, sickleavehours, currentflag)
+--VALUES ('20778', '245797968', 'adventure-works\sam0', 'Sales Representative', '1996-07-29', 'S', 'F', '2016-01-01', 1, '20', '38', 1)
+
+--insert into person.[address] (addressline1, city, stateprovinceid, postalcode)
+--values ('1234 test blvd', 'Bothell','79','98011')
+
+--Insert into person.BusinessEntityAddress (BusinessEntityID, addressid, addresstypeid)
+--values ('20778', (select addressid from person.[address] where addressline1 = '1234 test blvd'), 2)
+
+--insert into humanresources.EmployeeDepartmentHistory (businessentityid, departmentid, shiftid, startdate, enddate)
+--values ('20778','3', 1, '2016-01-01', '2017-01-01')
+
+--insert into humanresources.EmployeePayHistory (businessentityid, ratechangedate, rate, PayFrequency)
+--values ('20778', '2017-01-01', '125.50', 2)
+
+--Insert into Sales.SalesPerson (BusinessEntityID, territoryid, SalesQuota, bonus, CommissionPct, SalesYTD, SalesLastYear)
+--values ('20778', '2', '250000.00','2000.00','0.015','4251368.5497','1439156.0291')
+
+--insert into sales.SalesPersonQuotaHistory (businessentityid, quotadate, salesquota)
+--values 
+--('20778', '2017-01-01', '7000.00'),
+--('20778', '2017-04-01', '14000.00'),
+--('20778', '2017-07-01', '28000.00'),
+--('20778', '2017-10-01', '56000.00')
+
+--insert into sales.SalesTerritoryHistory (BusinessEntityID, TerritoryID, StartDate, EndDate)
+--values ('20778', '2', '2016-01-01', '2017-01-01')
+
+
+--COMMIT TRANSACTION
+
+--3
+
+--GO
+--  IF (Select COUNT(*) from INFORMATION_SCHEMA.tables t where t.TABLE_NAME = 'VendorIssues') > 0 
+-- BEGIN
+-- DROP TABLE VendorIssues
+-- END 
+
+--Create table Purchasing.VendorIssues 
+--(
+--ReportID INT Identity(1,1), 
+--PurchaseOrderID INT,
+--Entrydate date, 
+--issuedetails varchar(100), 
+--vendorresponse varchar(100),
+--resolved bit,
+--PRIMARY KEY (ReportID),
+--CONSTRAINT FK_VendorIssues_PurchaseOrderHeader FOREIGN KEY (PurchaseOrderID) REFERENCES purchasing.PurchaseOrderHeader (PurchaseOrderid)
+--)
+
+--SET IDENTITY_INSERT Adventureworks2012.Purchasing.VendorIssues ON 
+
+--Insert into purchasing.PurchaseOrderHeader (RevisionNumber, [status], employeeid, VendorID, ShipMethodID, orderdate, shipdate, subtotal, TaxAmt, freight)
+--values ('6', '2', '254', '1636', '3', '2018-07-25 00:00:00.000', '2018-08-19 00:00:00.000', '997680.00', '79814.40', '19953.60')
+
+
+--INSERT INTO Purchasing.VendorIssues (ReportID, Purchaseorderid, entrydate, issuedetails, vendorresponse, resolved)
+--VALUES ('5', '4013', '2018-01-30', 'test', 'test', 1)
+
+--SET IDENTITY_INSERT Adventureworks2012.Purchasing.VendorIssues OFF
+
+--INSERT INTO Purchasing.VendorIssues ( Purchaseorderid, entrydate, issuedetails, vendorresponse, resolved)
+--Values 
+--('4012', '2018-01-30', 'test2', 'test2', 1),
+--('324', '2018-01-30', 'test3', 'test3', 0),
+--('529', '2018-01-30', 'test4', 'test4', 1),
+--('52', '2018-01-30', 'test5', 'test5', 0)
+
+--4
+
+
+--ALTER VIEW AdventureWorks2012MailingList
+--AS
+
+--Select  P.Title, Firstname, Middlename, Lastname, EA.EmailAddress, PP.PhoneNumber, A.AddressLine1, A.City, A.PostalCode, CR.Name [Country]
+--FROM Sales.SalesPerson SP
+--INNER JOIN Person.Person P
+--ON P.BusinessEntityID = SP.BusinessEntityID
+--INNER JOIN Person.EmailAddress EA
+--ON EA.BusinessEntityID = SP.BusinessEntityID
+--INNER JOIN Person.PersonPhone PP
+--ON PP.BusinessEntityID = P.BusinessEntityID
+--INNER JOIN Person.BusinessEntityAddress BEA
+--ON BEA.BusinessEntityID = PP.BusinessEntityID
+--INNER JOIN Person.[Address] A
+--ON A.AddressID = BEA.AddressID
+--INNER JOIN Person.StateProvince PSP
+--ON A.StateProvinceID = PSP.StateProvinceID
+--INNER JOIN Person.CountryRegion CR
+--ON PSP.CountryRegionCode = CR.CountryRegionCode
+--INNER JOIN HumanResources.Employee E
+--ON E.BusinessEntityID = P.BusinessEntityID
+--WHERE E.currentflag = 1
+
+--WITH CHECK OPTION 
+
+
+--5
+
+--GO 
+
+--Create VIEW AdventureWorks2012MailingList2ElectricBoogaloo
+--AS
+
+--Select  P.Title, Firstname, Middlename, Lastname, EA.EmailAddress, PP.PhoneNumber, A.AddressLine1, A.City, A.PostalCode, CR.Name [Country]
+--FROM Sales.SalesPerson SP
+--INNER JOIN Person.Person P
+--ON P.BusinessEntityID = SP.BusinessEntityID
+--INNER JOIN Person.EmailAddress EA
+--ON EA.BusinessEntityID = SP.BusinessEntityID
+--INNER JOIN Person.PersonPhone PP
+--ON PP.BusinessEntityID = P.BusinessEntityID
+--INNER JOIN Person.BusinessEntityAddress BEA
+--ON BEA.BusinessEntityID = PP.BusinessEntityID
+--INNER JOIN Person.[Address] A
+--ON A.AddressID = BEA.AddressID
+--INNER JOIN Person.StateProvince PSP
+--ON A.StateProvinceID = PSP.StateProvinceID
+--INNER JOIN Person.CountryRegion CR
+--ON PSP.CountryRegionCode = CR.CountryRegionCode
+--INNER JOIN HumanResources.Employee E
+--ON E.BusinessEntityID = P.BusinessEntityID
+--WHERE E.currentflag = 0
+
+--WITH CHECK OPTION 
+
+--GO 
+
+--6
+
+--ALTER TABLE Humanresources.Employee
+
+--ADD Title Nvarchar(8),
+--Firstname NVARCHAR(50),
+--Middlename NVARCHAR(50),
+--LastName NVARCHAR(50)
+
+--GO
+
+--UPDATE Humanresources.Employee
+--SET Title = P.Title, Firstname = P.Firstname, MiddleName = P.Middlename, Lastname = P.Lastname
+--From Person.Person P
+--WHERE Humanresources.Employee.BusinessEntityID = P.BusinessEntityID
+
+
+--7
+
+--CREATE TABLE Humanresources.EmployeeAddresses
+--(
+--Businessentityid INT,
+--AddressLine1 VARCHAR(50),
+--AddressLine2 VARCHAR(50),
+--City VARCHAR(50),
+--[State] Varchar(50), 
+--Postalcode Nvarchar(50),
+--Country varchar (50)
+--PRIMARY KEY (Businessentityid)
+--)
+
+--select * from Humanresources.EmployeeAddresses
+
+--Insert INTO Humanresources.EmployeeAddresses (Businessentityid, AddressLine1, AddressLine2, City, [State], Postalcode, country)
+--Select P.businessentityid, A.Addressline1, A.Addressline2, A.City, PSP.Name, A.Postalcode,CR.Name
+--From Person.Person P
+--INNER JOIN Person.EmailAddress EA
+--ON EA.BusinessEntityID = P.BusinessEntityID
+--INNER JOIN Person.PersonPhone PP
+--ON PP.BusinessEntityID = P.BusinessEntityID
+--INNER JOIN Person.BusinessEntityAddress BEA
+--ON BEA.BusinessEntityID = PP.BusinessEntityID
+--INNER JOIN Person.[Address] A
+--ON A.AddressID = BEA.AddressID
+--INNER JOIN Person.StateProvince PSP
+--ON A.StateProvinceID = PSP.StateProvinceID
+--INNER JOIN Person.CountryRegion CR
+--ON PSP.CountryRegionCode = CR.CountryRegionCode
+--INNER JOIN HumanResources.Employee E
+--ON E.BusinessEntityID = P.BusinessEntityID
+--WHERE E.BusinessEntityID = P.BusinessEntityID
